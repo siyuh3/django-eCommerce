c3e1528d57cdd4ef504022e51a23c9d9e6149395
siyuh3 <meikoshans@gmail.com>
2023-02-11 14:03:29 -0800

    products page/main page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+
 def index(request):
     return render(request, 'index.html')
+
+
+def products(request):
+    return render(request, 'products.html')
+
+def cart(request):
+    return render(request, 'cart.html')
--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('products/', include('core.urls')),
+    path('cart/', include('core.urls'))
 ]
