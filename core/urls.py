3f71d425b3976b2d82816b745f634863fba0fb35
siyuh3 <meikoshans@gmail.com>
2023-02-13 22:34:47 -0800

    CRUD in cart finished

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
-    path('cart/', views.cart, name='cart')
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('update_item/', views.updateItem, name="checkout")
 ]
 
